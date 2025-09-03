8194b3a38fb0f686b13605b01b35ece9f5c452da
jahongir95 <jakhongirsagdullaev95@gmail.com>
2025-09-03 17:51:24 +0500

    safe area color

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -40,6 +40,7 @@ export function App() {
     WebApp.expand(); // to'liq ekranga kengaytiradi
     WebApp.enableClosingConfirmation(); // foydalanuvchi chiqib ketganda so'raydi
     WebApp.setHeaderColor("#ffffff");
+    WebApp.setBottomBarColor
   }, []);
 
   return (
