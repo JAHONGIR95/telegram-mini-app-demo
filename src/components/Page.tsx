703178facd7d3575748331294964fa90e41660ea
jahongir95 <jakhongirsagdullaev95@gmail.com>
2025-09-13 22:39:29 +0500

    close button

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,20 +1,60 @@
-import { useNavigate } from 'react-router-dom';
-import { hideBackButton, onBackButtonClick, showBackButton } from '@telegram-apps/sdk-react';
-import { type PropsWithChildren, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { hideBackButton, onBackButtonClick, showBackButton } from '@telegram-apps/sdk-react';
+// import { type PropsWithChildren, useEffect } from 'react';
 
-export function Page({ children, back = true }: PropsWithChildren<{
+// export function Page({ children, back = true }: PropsWithChildren<{
+//   /**
+//    * True if it is allowed to go back from this page.
+//    */
+//   back?: boolean
+// }>) {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     if (back) {
+//       showBackButton();
+//       return onBackButtonClick(() => {
+//         navigate(-1);
+//       });
+//     }
+//     hideBackButton();
+//   }, [back]);
+
+//   return <>{children}</>;
+// }
+
+import { useNavigate } from "react-router-dom";
+import {
+  hideBackButton,
+  onBackButtonClick,
+  onMainButtonClick,
+  showBackButton,
+} from "@telegram-apps/sdk-react";
+import { type PropsWithChildren, useEffect } from "react";
+
+export function Page({
+  children,
+  back = true,
+}: PropsWithChildren<{
   /**
    * True if it is allowed to go back from this page.
    */
-  back?: boolean
+  back?: boolean;
 }>) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (back) {
-      showBackButton();
-      return onBackButtonClick(() => {
-        navigate(-1);
+    if (window.history.length > 1) {
+      if (back) {
+        showBackButton();
+        return onBackButtonClick(() => {
+          navigate(-1);
+        });
+      }
+    } else {
+      window.Telegram?.WebApp?.MainButton?.setText("Close");
+      return onMainButtonClick(() => {
+        window.Telegram?.WebApp?.close();
       });
     }
     hideBackButton();
