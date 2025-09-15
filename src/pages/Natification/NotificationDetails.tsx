5cb0771176ef92eb4ffc2d74049f81b734b1610c
jahongir95 <jakhongirsagdullaev95@gmail.com>
2025-09-15 15:46:28 +0500

    safe-area-bottom on notifications

--- src/pages/Natification/NotificationDetails.tsx
+++ src/pages/Natification/NotificationDetails.tsx
@@ -2,8 +2,10 @@ import PostCard from "@/components/postCard";
 import mainLogo from "/images/main-logo.svg";
 import { Post, posts } from "@/utils/constantValues";
 import { useState } from "react";
+import { useSafeAreaBottom } from "@/components/App";
 
 const NotificationDetails = () => {
+  const { safeAreaTop, safeAreaBottom } = useSafeAreaBottom();
   const handlePostComment = (data: Post) => {
     console.log(data);
   };
@@ -15,7 +17,7 @@ const NotificationDetails = () => {
 
   return (
     <div className="h-screen bg-accent flex flex-col">
-      <div className="flex justify-center py-6 opacity-50">
+      <div className="flex justify-center pb-4 opacity-50" style={{ paddingTop: safeAreaTop }}>
         <img src={mainLogo} className="w-24 h-12" />
       </div>
 
@@ -23,7 +25,7 @@ const NotificationDetails = () => {
         Смысл от 20.02.2025
       </h1>
       <div className="">
-        <div className="px-3 pt-7 pb-20 overflow-y-auto">
+        <div className="px-3 pt-4 overflow-y-auto space-y-5" style={{ paddingBottom: safeAreaBottom + 80}}>
           {posts?.map((post) => (
             <PostCard
               key={post.id}
