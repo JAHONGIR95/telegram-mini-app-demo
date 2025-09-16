278f01866e0ebd68d1589120613ec6c0413ede96
jahongir95 <jakhongirsagdullaev95@gmail.com>
2025-09-16 15:31:26 +0500

    navigationni yashirish 5

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,6 +1,7 @@
 import { useSafeAreaBottom } from "@/components/App";
 import Button from "@/components/buttons/Button";
 import Header from "@/components/Header";
+import { useKeyboardOpen } from "@/hooks/useKeyboardOpen";
 import Books from "@/views/search/Books";
 import Connections from "@/views/search/Connections";
 import People from "@/views/search/People";
@@ -13,6 +14,7 @@ const Search = () => {
   const [activeCategory, setActiveCategory] = useState("all");
   const [isOpen, setIsOpen] = useState(false);
   const { safeAreaBottom } = useSafeAreaBottom();
+  const isKeyboardOpen = useKeyboardOpen();
 
   const buttons = [
     {
@@ -89,48 +91,53 @@ const Search = () => {
         </div>
 
         {/* Collapse */}
-        {activeSection === "books" && <div className="collapse mb-0 pb-0">
-          <input
-            type="checkbox"
-            onClick={() => setIsOpen((prev) => !prev)}
-            className="inline-block"
-          />
-
-          <p className="collapse-title font-extrabold text-sm leading-4 text-tertiary flex items-center gap-2">
-            Категории:{" "}
-            <span
-              className={clsx(
-                "icon arrow_down_icon transition-transform duration-400",
-                isOpen ? "-rotate-180" : "rotate-0"
-              )}
+        {activeSection === "books" && (
+          <div className="collapse mb-0 pb-0">
+            <input
+              type="checkbox"
+              onClick={() => setIsOpen((prev) => !prev)}
+              className="inline-block"
             />
-          </p>
-          <div className="collapse-content w-full overflow-x-auto no-scrollbar">
-            {/* <div className="w-full overflow-x-auto no-scrollbar"> */}
-            <div className="flex gap-2 w-max">
-              {categories?.map((button, idx) => (
-                <Button
-                  key={idx}
-                  variant={
-                    button.value === activeCategory ? "ghost" : "outline"
-                  }
-                  className={clsx(
-                    "rounded-sm p-2 text-xs",
-                    button.value === activeCategory &&
-                      "!bg-[#D8D8D8] text-[#1E1D1E]"
-                  )}
-                  onClick={() => setActiveCategory(button.value)}
-                >
-                  {button.label}
-                </Button>
-              ))}
+
+            <p className="collapse-title font-extrabold text-sm leading-4 text-tertiary flex items-center gap-2">
+              Категории:{" "}
+              <span
+                className={clsx(
+                  "icon arrow_down_icon transition-transform duration-400",
+                  isOpen ? "-rotate-180" : "rotate-0"
+                )}
+              />
+            </p>
+            <div className="collapse-content w-full overflow-x-auto no-scrollbar">
+              {/* <div className="w-full overflow-x-auto no-scrollbar"> */}
+              <div className="flex gap-2 w-max">
+                {categories?.map((button, idx) => (
+                  <Button
+                    key={idx}
+                    variant={
+                      button.value === activeCategory ? "ghost" : "outline"
+                    }
+                    className={clsx(
+                      "rounded-sm p-2 text-xs",
+                      button.value === activeCategory &&
+                        "!bg-[#D8D8D8] text-[#1E1D1E]"
+                    )}
+                    onClick={() => setActiveCategory(button.value)}
+                  >
+                    {button.label}
+                  </Button>
+                ))}
+              </div>
+              {/* </div> */}
             </div>
-            {/* </div> */}
           </div>
-        </div>}
+        )}
       </Header>
 
-      <div className="pt-3 overflow-y-auto px-3 space-y-3" style={{ paddingBottom: safeAreaBottom + 80 }}>
+      <div
+        className="pt-3 overflow-y-auto px-3 space-y-3"
+        style={{ paddingBottom: safeAreaBottom + (isKeyboardOpen ? 0 : 80) }}
+      >
         {activeSection === "books" && <Books search={search} />}
         {activeSection === "people" && <People search={search} />}
         {activeSection === "connections" && <Connections search={search} />}
